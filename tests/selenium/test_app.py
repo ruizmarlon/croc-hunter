46239a71dba54b0a722f3577e57449b05914c120
jldeen <jedeen@outlook.com>
2020-03-02 19:24:30 -0800

    revert testing

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -10,43 +10,6 @@ from selenium.common.exceptions import (
     NoSuchElementException,
     WebDriverException)
 
-# applitools env vars
-
-hostname = os.getenv('INGRESS_HOSTNAME')
-appli_api_key = os.getenv('APPLI_API_KEY')
-
-from applitools.selenium import Eyes, Target
-
-eyes = Eyes()
-
-    # Initialize the eyes SDK and set your private API key.
-    eyes.api_key = os.environ['APPLI_API_KEY']
-
-    try:
-
-        # Open a Chrome browser.
-        driver = webdriver.Chrome()
-
-        # Start the test and set the browser's viewport size to 800x600.
-        eyes.open(driver, "Test app", "First test", {'width': 800, 'height': 600})
-
-        # Navigate the browser to the "hello world!" web-site.
-        driver.get('INGRESS_HOSTNAME')
-
-        # Visual checkpoint #1.
-        eyes.check("Login Window test", Target.window())
-
-        # End the test.
-        eyes.close()
-
-    finally:
-
-        # Close the browser.
-        driver.quit()
-
-        # If the test was aborted before eyes.close was called, ends the test as aborted.
-        eyes.abort()
-
 # Codefresh env vars
 hostname = os.getenv('INGRESS_HOSTNAME')
 release_name = os.getenv('RELEASE_NAME')
