7754235a7d24edb52609f376f18bd8100aef659f
jldeen <jedeen@outlook.com>
2020-05-19 20:18:26 -0700

    remv visual tests

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -9,10 +9,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import (
     NoSuchElementException,
     WebDriverException)
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from applitools.selenium import Eyes, Target
 
-# Codefresh env vars
 hostname = os.getenv('INGRESS_HOSTNAME')
 release_name = os.getenv('RELEASE_NAME')
 commit_sha = os.getenv('CF_SHORT_REVISION')
@@ -20,32 +17,6 @@ commit_sha = os.getenv('CF_SHORT_REVISION')
 # Give Selenium Hub time to start
 time.sleep(15)  # TODO: figure how to do this better
 
-
-# # <-- Begin Applitools Visual Testing --> 
-# eyes = Eyes()
-# # Set applitools api key
-# eyes.api_key = os.getenv('APPLITOOLS_API_KEY')
-# # Set Browser Object for Applitools
-# browser_name = ip = os.getenv('BROWSER')
-# browser = webdriver.Remote(
-#           command_executor='http://selenium_hub:4444/wd/hub',
-#         #   desired_capabilities=DesiredCapabilities.CHROME
-#           desired_capabilities={'browserName': browser_name},
-# )
-# # Open Eyes         
-# eyes.open(browser, "Test app", "Croc hunter test", {'width': 800, 'height': 600})
-# # Open Browser
-# browser.get("https://{}".format(hostname))
-# # Take snapshot
-# eyes.check("Login Window test", Target.window())
-# # Close Eyes
-# eyes.close()
-# # Quit Browser
-# browser.quit()
-# # <-- End Applitools Visual Testing -->
-
-
-# <-- Begin pytest with Selenium -->
 @pytest.fixture(scope='module')
 def browser():
     browser_name = ip = os.getenv('BROWSER')
@@ -56,7 +27,7 @@ def browser():
     yield browser
     browser.quit()
 
-    
+
 def test_confirm_title(browser):
     browser.get("https://{}".format(hostname))
     assert "Croc Hunter" in browser.title
@@ -96,4 +67,3 @@ def test_confirm_commit_sha(browser):
     browser.get("https://{}".format(hostname))
     element = browser.find_element(By.XPATH, '//div[@class="details"]')
     assert commit_sha in element.text
-# <-- End pytest with Selenium -->
